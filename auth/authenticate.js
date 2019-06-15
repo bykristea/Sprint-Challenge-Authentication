c58e62a5627386cf318b75fa78a3c88a1fd883f3
Kristea Gamel <krgamel@gmail.com>
2019-06-15 14:19:36 -0500

    I think MVP met
    pretty frustrating day.

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets.js');
 
+const jwtKey =
+  process.env.JWT_SECRET ||
+  'add a .env file to root of project with the JWT_SECRET variable';
 // quickly see what this file exports
 module.exports = {
   authenticate,
@@ -12,7 +14,7 @@ function authenticate(req, res, next) {
   const token = req.get('Authorization');
 
   if (token) {
-    jwt.verify(token, secrets.jwtSecret, (err, decoded) => {
+    jwt.verify(token, jwtKey, (err, decoded) => {
       if (err) return res.status(401).json(err);
 
       req.decoded = decoded;
@@ -37,5 +39,5 @@ function generateToken(user) {
     jwtid: '12345'
   }
 
-  return jwt.sign(payload, secrets.jwtSecret, options);
+  return jwt.sign(payload, jwtKey, options);
 }
--- config/routes.js
+++ config/routes.js
@@ -48,6 +48,7 @@ function login(req, res) {
     })
 }
 
+
 function getJokes(req, res) {
   const requestOptions = {
     headers: { accept: 'application/json' },
