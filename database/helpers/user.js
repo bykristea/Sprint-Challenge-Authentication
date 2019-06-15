ad8d0738c971bf703046b4147e23b0cfb5a2256b
Kristea Gamel <krgamel@gmail.com>
2019-06-15 11:56:56 -0500

    created helper for routes

---
+++ database/helpers/user.js
@@ -0,0 +1,14 @@
+const knex = require('knex');
+const dbConfig = require('../../knexfile.js');
+const db = knex(dbConfig.development);
+
+
+// helper function that inserts user upon registration
+module.exports = {
+    add: (user) => {
+        return db('users').insert(user);
+    }
+
+   
+
+};
